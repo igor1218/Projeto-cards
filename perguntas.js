e2c45cea0cc327bb41e55be45ec4eaff2030a255
igor1218 <igor.silva12@escola.pr.gov.br>
2024-11-26 08:15:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,64 @@
+criaCartao(
+    'Clash royale',
+    'quanto elixir custa a Pekka?',
+    '7 elixir'
+)
+
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o mk?',
+    '7 elixir'
+)
+
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o tronco?',
+    '2 elixir'
+)
+
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa a gangue de goblins?',
+    '3 elixir '
+)
+
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o barril de barbaro?',
+    '2 elixir'
+)
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o exercito de esqueletos?',
+    ' 3 elixir'
+)
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o sparky?',
+    '6 elixir'
+)
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o dragao infernal?',
+    '4 elixir'
+)
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o fantasma real?',
+    '3 elixir'
+)
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o gigante?',
+    '5 elixir'
+)
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o mineiro?',
+    '3 elixir'
+)
+criaCartao(
+    'Clash royale',
+    'Quanto elixir custa o mago?',
+    '5 elixir'
+)
